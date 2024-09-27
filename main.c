0200bb66afc5717816691a8575362cd682b4350e
MichelElRaheb <msfh187@uowmail.edu.au>
2024-09-27 12:46:16 +0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+# define coffee_beans 8
+# define water 30
+# define waterM 39
+# define milkC 70
+# define milkM 160
+# define CS 30
+# define ADpass 1235
+
+//Minimum ingrdients before alerted 
+# define CBmin 10
+# define watermin 50
+# define milkmin 200
+# define CSmin 40
+
+int refill(int a);
+int refillW(int );
+int main()
+
+
+
+int refill(int a)
+{
+    if coffee_beans <= CBmin;
+    printf("Refill coffee beans.");
+    else if ;
+    return 0;
+}
+int refillW(int )
+{ 
+    if waterM <= watermin;
+    printf("Refill water.");
+    else if;
+    return 0;
+}
